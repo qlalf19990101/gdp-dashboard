cb80de1cc329f42277667b4cd40a9b1741f2f255
qlalf19990101 <qlalf19990101@gmail.com>
2024-07-14 06:45:58 +0900

    Update 뉴스기사앱_중간.py

--- 뉴스기사앱_중간.py
+++ 뉴스기사앱_중간.py
@@ -122,6 +122,7 @@ def page2():
     if df_filtered.empty:
         st.write("검색 결과가 없습니다.")
         total_pages = 1
+        return
     else: # 검색 결과가 있는 경우에만 정렬
         if criteria == '조회수':
             df_filtered = df_filtered.sort_values(by='조회수', ascending=False)
