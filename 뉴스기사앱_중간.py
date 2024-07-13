c7e11e3cb8007cbb10317f2f09e10cf77305ff94
qlalf19990101 <qlalf19990101@gmail.com>
2024-07-14 05:45:41 +0900

    Update 뉴스기사앱_중간.py

--- 뉴스기사앱_중간.py
+++ 뉴스기사앱_중간.py
@@ -124,37 +124,36 @@ def page2():
     elif criteria == '순위':
         df_filtered = df_filtered.sort_values(by='순위')
 
+    
     # 페이지네이션
     page_size = 20
     total_pages = (len(df_filtered) + page_size - 1) // page_size  # 전체 페이지 수 계산
-
+    
     if 'page_number' not in st.session_state:
         st.session_state['page_number'] = 1  # 현재 페이지 번호를 세션 상태에 저장
-
+    
     col1, col2, col3 = st.columns([1, 3, 1])  # 페이지 이동 버튼을 위한 레이아웃
-
-    # 페이지 번호 입력 및 이동
-    go_to_page = col1.number_input("페이지 번호 입력:", min_value=1, max_value=total_pages, value=st.session_state['page_number'])
-    if col1.button("이동"):
-        st.session_state['page_number'] = go_to_page
-        st.experimental_rerun()
-
-    # 페이지 번호 중앙 정렬 
-    col2.markdown(f'<div style="text-align: center;">페이지: {st.session_state["page_number"]} / {total_pages}</div>', unsafe_allow_html=True)
-
+    
     if st.session_state['page_number'] > 1:  # 1페이지에서는 이전 페이지 버튼 숨김
-        if col3.button("이전 페이지"):
+        if col1.button("이전 페이지"):
             st.session_state['page_number'] -= 1
             st.experimental_rerun()  # 페이지 새로고침
-
+    
+    # 페이지 번호 입력 필드 추가 (col1에 추가)
+    st.session_state['page_number'] = col1.number_input("페이지 번호 입력:", min_value=1, max_value=total_pages, value=st.session_state['page_number'], step=None)
+    
+    # 페이지 번호 중앙 정렬 (st.columns 내부로 이동)
+    col2.markdown(f'<div style="text-align: center;">페이지: {st.session_state["page_number"]} / {total_pages}</div>', unsafe_allow_html=True)
+    
     if st.session_state['page_number'] < total_pages:
         if col3.button("다음 페이지"):
             st.session_state['page_number'] += 1
             st.experimental_rerun()  # 페이지 새로고침
-
+    
     start_index = (st.session_state['page_number'] - 1) * page_size
     end_index = start_index + page_size
     df_page = df_filtered.iloc[start_index:end_index]
+    
 
      # 스타일 적용 및 인덱스 재설정
     df_styled = df_page.reset_index(drop=True).style.format({'링크': make_clickable})
