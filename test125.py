87af7cd4e1ec10fac3ecaca4380a2307d0abb5bd
qlalf19990101 <qlalf19990101@gmail.com>
2025-01-25 16:46:54 +0900

    Update test125.py

--- test125.py
+++ test125.py
@@ -137,7 +137,7 @@ elif input_method == "레시피별 재료 입력":
         spell_essence = get_input("spell_essence", 0.0)
 
         st.subheader("고급 보스킬러의 비약")
-        boss_potion = st.number_input("보스킬러의 비약 한 병의 가격 (단위: 만 메소)", min_value=0.0, step=0.1, value=boss_potion)
+        boss_potion = st.number_input("고급 보스킬러의 비약 한 병의 가격 (단위: 만 메소)", min_value=0.0, step=0.1, value=boss_potion)
         boss_base = st.number_input("보스킬러의 비약 한 병의 가격 (단위: 만 메소)", min_value=0.0, step=0.1, value=boss_base)
         hisop = st.number_input("히솝 꽃 한 개의 가격 (단위: 만 메소)", min_value=0.0, step=0.1, value=hisop)
         twilight_essence = st.number_input("영롱한 황혼의 정수 한 개의 가격 (단위: 만 메소)", min_value=0.0, step=0.1, value=twilight_essence)
